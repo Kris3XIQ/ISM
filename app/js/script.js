815023c91b3498db696159ac079f85b12ff5f958
Kris3XIQ <Kristoffer.markow@gmail.com>
2021-09-20 15:59:13 +0200

    Remade light/dark-mode switch in the navbar, changed .js

--- app/js/script.js
+++ app/js/script.js
@@ -1,58 +1,58 @@
-const darkButton = document.getElementById('dark');
-const lightButton = document.getElementById('light');
+// const darkButton = document.getElementById('dark');
+// const lightButton = document.getElementById('light');
 
-const setColorMode = () => {
-    if (localStorage.getItem('colorMode') == 'dark') {
-        setDarkMode();
-        darkButton.click();
-    } else if (localStorage.getItem('colorMode') == 'light') {
-        setLightMode();
-        lightButton.click();
-    }
-};
+// const setColorMode = () => {
+//     if (localStorage.getItem('colorMode') == 'dark') {
+//         setDarkMode();
+//         darkButton.click();
+//     } else if (localStorage.getItem('colorMode') == 'light') {
+//         setLightMode();
+//         lightButton.click();
+//     }
+// };
 
-const checkMode = () => {
-    if (localStorage.getItem('colorMode') == null) {
-        if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-            lightButton.click();
-        } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            darkButton.click();
-        }
-    }
-};
+// const checkMode = () => {
+//     if (localStorage.getItem('colorMode') == null) {
+//         if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+//             lightButton.click();
+//         } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//             darkButton.click();
+//         }
+//     }
+// };
 
-const checkModeChange = () => {
-    window
-        .matchMedia('(prefers-color-scheme: dark)')
-        .addEventListener('change', (event) => {
-        checkMode();
-    });
-};
+// const checkModeChange = () => {
+//     window
+//         .matchMedia('(prefers-color-scheme: dark)')
+//         .addEventListener('change', (event) => {
+//         checkMode();
+//     });
+// };
 
-const setDarkMode = () => {
-    document.querySelector('body').classList = 'dark';
-};
+// const setDarkMode = () => {
+//     document.querySelector('body').classList = 'dark';
+// };
 
-const setLightMode = () => {
-    document.querySelector('body').classList = 'light';
-};
+// const setLightMode = () => {
+//     document.querySelector('body').classList = 'light';
+// };
 
-setColorMode();
-checkMode();
-checkModeChange();
+// setColorMode();
+// checkMode();
+// checkModeChange();
 
-const radioButtons = document.querySelectorAll('.toggle__wrapper input');
-for (let i = 0; i < radioButtons.length; i++) {
-    radioButtons[i].addEventListener('click', (event) => {
-        if (darkButton.checked) {
-            localStorage.setItem('colorMode', 'dark');
-            setDarkMode();
-        } else {
-            localStorage.setItem('colorMode', 'light');
-            setLightMode();
-        }
-    });
-}
+// const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+// for (let i = 0; i < radioButtons.length; i++) {
+//     radioButtons[i].addEventListener('click', (event) => {
+//         if (darkButton.checked) {
+//             localStorage.setItem('colorMode', 'dark');
+//             setDarkMode();
+//         } else {
+//             localStorage.setItem('colorMode', 'light');
+//             setLightMode();
+//         }
+//     });
+// }
 
 // Navbar Functionality
 const navbarScrollFunctionality = () => {
@@ -64,6 +64,53 @@ const navbarScrollFunctionality = () => {
         window.scrollY > 0 ? (headerLogo[0].style.display = "none") : (headerLogo[0].style.display = "");
     
     });
+}
+
+const checkModeChange = () => {
+    window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', (event) => {
+        checkMode();
+    });
+};
+
+const setColorMode = () => {
+    if (localStorage.getItem('colorMode') == 'dark') {
+        setDarkMode();
+        darkButton.click();
+    } else if (localStorage.getItem('colorMode') == 'light') {
+        setLightMode();
+        lightButton.click();
+    }
+};
+
+const switchLightDarkMode = () => {
+    const body = document.querySelector('body');
+    const darkButton = document.getElementById('dark');
+    const lightButton = document.getElementById('light');
+    const themeSwitch = document.getElementById('theme-switch-container');
+    const userPreferDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userPreferLightMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+    if(userPreferDarkMode){
+        console.log("User prefers a dark interface");
+        body.classList = "dark";
+    }
+    if(userPreferLightMode){
+        console.log("User prefers a light interface");
+        body.classList = "light";
+    }
+    themeSwitch.addEventListener('click', (e) => {
+        toggleTheme();
+    })
+    const toggleTheme = () => {
+        if (body.classList.contains("dark")) {
+            body.classList.toggle("dark");
+            body.classList.toggle("light");
+        } else if (body.classList.contains("light")) {
+            body.classList.toggle("light");
+            body.classList.toggle("dark");
+        }
+    }
 }
 
 // Slideshow Functionality
@@ -121,5 +168,6 @@ const scrollToTop = () => {
     document.documentElement.scrollTop = 0; //Chrome, Firefox, IE & Opera
 }
 
+switchLightDarkMode();
 navbarScrollFunctionality();
 HomePageSlider();
